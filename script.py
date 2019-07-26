b8dd27cf3871e2d86b5a1745e42e34dcb2e5b12b
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2019-07-26 15:24:49 +0530

    fetching commit date&time

--- script.py
+++ script.py
@@ -1,12 +1,11 @@
 import requests
 import json
+import objectpath
 response = requests.get('https://api.github.com/repos/pratz0499/web-scraping/commits?since')
 res=response.text
 parsed=json.loads(res)
-print(json.dumps(parsed,indent=4,sort_keys=True))
-#print(parsed["date"])
-#for i in parsed["date"]:
-	#print(i)
-for i in range(len(parsed)):
-	for k,v in i.items():
-		print(k,v)
+#print(json.dumps(parsed,indent=4,sort_keys=True))
+json_tree=objectpath.Tree(parsed)
+commit_time=tuple(json_tree.execute('$..date'))
+for i in commit_time:
+	print(i)
