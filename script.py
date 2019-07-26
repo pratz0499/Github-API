12947e00023548307c1d6edf18b29df76edb5920
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2019-07-26 14:13:02 +0530

    json response of the commits

--- script.py
+++ script.py
@@ -1,3 +1,6 @@
 import requests
-response = requests.get('https://api.github.com/repos/pratz0499/web-scraping/commits')
-print(response.text)
+import json
+response = requests.get('https://api.github.com/repos/pratz0499/web-scraping/commits?since')
+res=response.text
+parsed=json.loads(res)
+print(json.dumps(parsed,indent=4,sort_keys=True))
