b4b628781fa4abf8f9f15f89bd8cda6573f6644b
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2019-07-26 15:15:25 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,3 +4,9 @@ response = requests.get('https://api.github.com/repos/pratz0499/web-scraping/com
 res=response.text
 parsed=json.loads(res)
 print(json.dumps(parsed,indent=4,sort_keys=True))
+#print(parsed["date"])
+#for i in parsed["date"]:
+	#print(i)
+for i in range(len(parsed)):
+	for k,v in i.items():
+		print(k,v)
