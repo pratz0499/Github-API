feb559e6ef26a1afab2b1c6911284b89f4320034
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2019-09-28 18:07:09 +0530

    Update bitbucket.py

--- bitbucket.py
+++ bitbucket.py
@@ -1,4 +1,4 @@
-curl -s "https://api.bitbucket.org/2.0/repositories/bmsce2019ccplabpb/repositories/?page=1"> temp.txt
+curl -s "https://api.bitbucket.org/2.0/repositories/bmsce2019ccplabpb/?page=1"> temp.txt
 
 fp=open("temp.txt")
 x=json.load(fp)
