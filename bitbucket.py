0a8571f4c4738f861c3e154ec65cf53d9bd41f8f
pratz0499 <36686740+pratz0499@users.noreply.github.com>
2019-09-28 18:05:04 +0530

    Create bitbucket.py

---
+++ bitbucket.py
@@ -0,0 +1,6 @@
+curl -s "https://api.bitbucket.org/2.0/repositories/bmsce2019ccplabpb/repositories/?page=1"> temp.txt
+
+fp=open("temp.txt")
+x=json.load(fp)
+for i in x['values']:
+           print(i['links']['clone'][0]['href'])
